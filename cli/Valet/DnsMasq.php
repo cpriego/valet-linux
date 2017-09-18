33920a48ce81fc282cfd1ae9056024e691752e1b
nickurt <nickurt@users.noreply.github.com>
2017-09-18 13:28:50 +0200

    changed the default tld from dev to test

--- cli/Valet/DnsMasq.php
+++ cli/Valet/DnsMasq.php
@@ -43,7 +43,7 @@ class DnsMasq
     {
         $this->dnsmasqSetup();
         $this->fixResolved();
-        $this->createCustomConfigFile('dev');
+        $this->createCustomConfigFile('test');
         $this->pm->dnsmasqRestart($this->sm);
     }
 
